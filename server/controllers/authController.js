5236dbbe7a50b0abade7a604f02310f391d4e353
Ryan Huie <rhpurple1023@gmail.com>
2022-08-29 17:02:39 -0700

    Added authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,19 +1,58 @@
-const db = require('../model/model.js')
+const db = require('../model/model.js');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 module.exports = {
-//   All general api controllers will go here
-  testController: async (req, res, next) => {
+  // look into why catch block doesn't catch db errror ... potentially b/c nested within bcrypt callback
+  checkSignup: async (req, res, next) => {
+    const { username, password, email } = await req.body;
+    if(!username || !password || !email) res.status(422).send({error: 'You must provide a username, password and email.'});
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+      if(err) return next({ message:'There was an error with the signup request' });
+      const accountsQuery = 'INSERT INTO accounts (username, password, email) VALUES ($1, $2, $3) RETURNING *';
+      const accountsQueryParams = [username, hash, email];
+      db.query(accountsQuery, accountsQueryParams).then(() => {
+        res.locals.username = username;
+        res.locals.hash = hash;
+        res.locals.email = email;
+        res.locals.userCreated = true;
+        next();
+      }).catch((err) => {
+        console.log(err);
+        res.locals.userCreated = false;
+        return next();
+      })
+    });
+  },
+
+  checkLogin: async (req, res, next) => {
     try {
-      console.log('test')
-      const data = await db.query('SELECT * FROM "public"."post_table" LIMIT 100')
-      console.log(data)
-      next()
+      const { username, password, email } = await req.body;
+      const accountsQuery = 'SELECT * FROM accounts WHERE username = $1';
+      const accountsQueryParams = [ username ];
+
+      const data = await db.query(accountsQuery, accountsQueryParams);
+      const userPass = await data.rows[0].password;
+      await bcrypt.compare(password, userPass, (err, response) => {
+          if(err) {
+              return next(err);
+          }
+
+          if(response === false){
+              res.locals.isUser = false;
+          } else {
+            res.locals.username = username;
+            res.locals.hash = userPass;
+            res.locals.email = email;
+            res.locals.isUser = true;
+          }
+          return next();
+      });
     } catch(err) {
-      console.log(err)
-      next(err)
+      res.locals.isUser = false;
+      next()
     }
   }
   
 }
-
-// send back email on login
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -3,29 +3,31 @@ const authController = require('../controllers/authController.js');
 const searchDBController = require('../controllers/searchDBController');
 const updateDBController = require('../controllers/updateDBController.js');
 
-const { addToBook_Table, getBook_id, addToPost_Table, addToHash_Table, addToRating_Table, addToPHJ_BHJ } = updateDBController;
+const { checkSignup, checkLogin } = authController;
 const { getPostInfo, getBookInfo, getRatingsInfo, getHashIDs, getTagInfo } = searchDBController;
+const { addToBook_Table, getBook_id, addToPost_Table, addToHash_Table, addToRating_Table, addToPHJ_BHJ } = updateDBController;
 
-const router = express.Router()
 
-router.post('/login', (req, res) => {
-  res.send('post login')
-})
+const router = express.Router();
 
-router.post('/signup', (req, res) => {
-  res.send('post signup')
-})
+router.post('/login', checkLogin, (req, res) => {
+  res.status(200).json(res.locals);
+});
+
+router.post('/signup', checkSignup, (req, res) => {
+  res.status(200).json(res.locals);
+});
 
 router.get('/reviews/', getPostInfo, getBookInfo, getRatingsInfo, getHashIDs, getTagInfo, (req, res) => {
-  return res.status(200).json(res.locals.reviews)
-})
+  return res.status(200).json(res.locals.reviews);
+});
 
 router.get('/reviews/:username', getPostInfo, getBookInfo, getRatingsInfo, getHashIDs, getTagInfo, (req, res) => {
-  return res.status(200).json(res.locals.reviews)
+  return res.status(200).json(res.locals.reviews);
 })
 
 router.post('/reviews/:username', addToBook_Table, getBook_id, addToPost_Table, addToHash_Table, addToRating_Table, addToPHJ_BHJ, (req, res) => {
   return res.status(200).json("post review");
-})
+});
 
-module.exports = router
+module.exports = router;
