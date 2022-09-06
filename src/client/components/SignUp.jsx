bba2761b768ce7743fe136bb819112a2bdba6f14
shelbycarlin <shelbycarlin13@gmail.com>
2022-09-06 13:41:36 -0700

    added some elements for the signup page still need to do state managment and requests to the back end

--- src/client/components/SignUp.jsx
+++ src/client/components/SignUp.jsx
@@ -1,14 +1,23 @@
 import React, { useRef } from 'react';
 // import { useNavigate } from 'react-router-dom';
 // import { useDispatch } from 'react-redux';
-// import InputField from './InputField.jsx';
+ import InputField from './InputField.jsx';
 //import { signUp } from '../actions/actions';
 //import '../../styles/SignUp.scss'
 
 const SignUp = ()=> {
+  const usernameRef = useRef(null);
+  //declare passwordRef assign to useRef(null)
+  const passwordRef = useRef(null);
+  const emailRef = useRef(null);
+
   return(
     <div>
-      <h1>SignUp</h1>
+      <h1>Sign Up</h1>
+      <InputField id='username' label='Username: ' ref={usernameRef}/>
+      <InputField id='password' label='Password: ' inputType='password' ref={passwordRef} />
+      <InputField id='email' label='Email: ' ref={emailRef}/>
+      <button>Sign Up</button>
     </div>
   )
 }
